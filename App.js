6b5128dee283bb7493a2de157e4802650abb16b7
Debasish Gracias <debzu57@gmail.com>
2021-11-16 18:17:32 +0000

    implement account and listing screen

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React,{ useState } from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  const [category, setCategory] = useState();
+  return (
+    <Screen>
+     <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
